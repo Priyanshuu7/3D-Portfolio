7668e8bdd43a073af8872cfb2b6616fe7a460f26
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-10-05 23:58:24 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,11 +41,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text=[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled web developer with experince in javascript, and expertise
-        in frameworks like React ,Node.js and Three.js I'm a quick learner and
-        collaborate closely with the clients to create efficient scalable and
-        user-friendly solutions that solve real world problems .Let's work
-        togethor to bring your ideas to life
+        I'm a skilled web developer with experience in JavaScript and expertise
+        in frameworks like React, Node.js, and Three.js. I'm a quick learner and
+        collaborate closely with clients to create efficient, scalable, and
+        user-friendly solutions that solve real-world problems. Let's work
+        together to bring your ideas to life.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
