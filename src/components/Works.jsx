3366d72bdcd9387c1df304368f1fcdda68cfe162
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-03-21 22:20:11 +0530

    Update work.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -72,10 +72,11 @@ const Works = () => {
         <p className={styles.sectionSubText}>My Work</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
+
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-4 text-secondary text=[17px] w-full leading-[30px] text-justify"
+          className="mt-4 text-secondary text-[17px] w-full leading-[30px] text-justify"
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
@@ -84,9 +85,14 @@ const Works = () => {
           and manage projects effectively.
         </motion.p>
       </div>
+
       <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
         ))}
       </div>
     </>
