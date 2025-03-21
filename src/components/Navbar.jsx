f1f492d80b89decd1ca07795b00217866ffd7797
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-03-21 22:35:35 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
@@ -20,7 +20,6 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
           <p className="text-white text-[18px] font-bold cursor- pointer flex">
             Priyanshu Rajak &nbsp;
             <span className="sm:block hidden">| WEB Developer</span>
